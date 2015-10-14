7ccf4707357051492d83dcba8bb75a843f3ecf47
Matthew Jackowski <mattjjacko@gmail.com>
2015-10-13 20:35:03 -0700

    fixes to javascript encodes

--- includes/transifex-live-integration-javascript.php
+++ includes/transifex-live-integration-javascript.php
@@ -32,18 +32,19 @@ class Transifex_Live_Integration_Javascript {
 	 */
 	function render() {
 		Plugin_Debug::logTrace();
-
-		$live_settings_string = json_encode( $this->live_settings );
+		
 		if ( $this->is_detectlang ) {
 			$lang = get_query_var( 'lang' );
 			$detectlang = <<<DETECTLANG
-function() { return '$lang';}
+function() { return "$lang";}
 DETECTLANG;
-			$this->live_settings = array_merge( $this->live_settings, array( 'detectlang' => $detectlang ) );
-			Plugin_Debug::logTrace( $this->live_settings );
+			$this->live_settings = array_merge( $this->live_settings, array( 'detectlang' => '%function%' ) );
+			
 		}
+		$live_settings_string = json_encode( $this->live_settings );
+		$live_settings_string = str_replace('"%function%"', $detectlang, $live_settings_string);
 		$include = <<<LIVE
-<script type="text/javascript">window.liveSettings=$live_settings_string;</script>
+<script type="text/javascript">window.liveSettings={"api_key":"d2581d15aa3844d9858dc1e8913b63bd","staging":0,"enable_frontend_css":0,"custom_picker_id":"","detectlang":function() { return "fr";}};</script>
 <script type="text/javascript" src="//cdn.transifex.com/live.js"></script>
 LIVE;
 		echo $include;
