68d29a573ddd3a8a73bb15acf4ae3dbc69b5d0ff
Matthew Jackowski <mattjjacko@gmail.com>
2016-01-12 20:44:18 -0800

    initial commit of full acceptance tests

--- tests/acceptance/PluginUICept.php
+++ tests/acceptance/PluginUICept.php
@@ -19,3 +19,26 @@ $I->seeInFormFields('#settings_form',[
 	'transifex_live_colors[menu]' => '#eaf1f7',
 	'transifex_live_colors[languages]' => '#666666'
 	]);
+$I->fillField('transifex_live_settings[api_key]','2699bc66df6546008d0a14acf26732a1');
+$I->click('Save Changes');
+$I->see('Your changes to the settings have been saved!');
+$I->see('Your changes to the colors have been saved!');
+$I->seeInFormFields('#settings_form',[ 'transifex_live_settings[add_language_rewrites]' => 'none']);
+$I->seeInFormFields('#settings_form',[ 'transifex_live_settings[hreflang]' => '0']);
+$I->seeInFormFields('#settings_form',[ 'transifex_live_settings[source_language]' => 'en']);
+$I->seeInFormFields('#settings_form',[ 
+	'transifex_live_settings[wp_language_fr]' => 'fr',
+	'transifex_live_settings[wp_language_de_DE]' => 'de_DE',
+	'transifex_live_settings[wp_language_ko]' => 'ko',
+	'transifex_live_settings[wp_language_es]' => 'es',
+	]);
+$I->click('Save Changes');
+$I->see('Your changes to the settings have been saved!');
+$I->see('Your changes to the colors have been saved!');
+$I->amOnPage('/');
+$I->seeInSource('<script type="text/javascript" src="//cdn.transifex.com/live.js"></script>');
+$I->seeInSource('<script type="text/javascript">window.liveSettings={"api_key":"2699bc66df6546008d0a14acf26732a1","enable_frontend_css":0,"custom_picker_id":""};</script>');
+
+
+
+
