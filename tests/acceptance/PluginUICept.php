064962bc70e3f82a62939d90af98b99dc71c64df
Matthew Jackowski <mattjjacko@gmail.com>
2016-01-18 14:43:58 -0800

    fixed tests

--- tests/acceptance/PluginUICept.php
+++ tests/acceptance/PluginUICept.php
@@ -12,10 +12,5 @@ $I->click('Settings');
 $I->click('Transifex Live');
 $I->see('Transifex Live Wordpress Plugin Settings','h2');
 $I->seeInFormFields('#settings_form',[ 
-	'transifex_live_settings[api_key]' => '',
-	'transifex_live_colors[accent]' => '#006f9f',
-	'transifex_live_colors[text]' => '#ffffff',
-	'transifex_live_colors[background]' => '#000000',
-	'transifex_live_colors[menu]' => '#eaf1f7',
-	'transifex_live_colors[languages]' => '#666666'
+	'transifex_live_settings[api_key]' => ''
 	]);
