bc4b68e047e378aae4e6e56387c3c45ecb5eb747
Matthew Jackowski <mattjjacko@gmail.com>
2016-03-24 16:14:30 -0700

    Updated tests

--- tests/unit/PrerenderCheckTest.php
+++ tests/unit/PrerenderCheckTest.php
@@ -11,31 +11,37 @@ class PrerenderCheckTest extends \PHPUnit_Framework_TestCase {
 		$settings = Transifex_Live_Integration_Defaults::settings();
 		$whitelist = $settings['whitelist_crawlers'];
 		$bot_types = $settings['generic_bot_types'];
-		$this->data = [[
+		$this->data = [[ // Standard Chrome Agent
 		'agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
 		'fragment' => NULL,
 		'bot_types' => $bot_types,
 		'whitelist' => $whitelist,
 		'result' => false
-			], [
+			],[ // Standard Firefox Agent
+		'agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:46.0) Gecko/20100101 Firefox/46.0',
+		'fragment' => NULL,
+		'bot_types' => $bot_types,
+		'whitelist' => $whitelist,
+		'result' => false
+			], [ // Prerender added
 				'agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36 Prerender (+https://github.com/prerender/prerender)',
 				'fragment' => NULL,
 				'bot_types' => $bot_types,
 				'whitelist' => $whitelist,
 				'result' => false
-			], [
+			], [ // Standard Googlebot  
 				'agent' => 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
 				'fragment' => NULL,
 				'bot_types' => $bot_types,
 				'whitelist' => $whitelist,
 				'result' => true
-			], [
+			], [ // Googlebot with prerender added
 				'agent' => 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html) Prerender (+https://github.com/prerender/prerender)',
 				'fragment' => NULL,
 				'bot_types' => $bot_types,
 				'whitelist' => $whitelist,
 				'result' => false
-			], [
+			], [ // some fragment
 				'agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
 				'fragment' => '/some-frament',
 				'bot_types' => $bot_types,
