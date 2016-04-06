6552a33cd93ad599de29334ea2f14cb276924abe
Matthew Jackowski <mattjjacko@gmail.com>
2016-04-06 11:37:54 -0700

    fixed escaped fragment with no val

--- includes/transifex-live-integration-static-factory.php
+++ includes/transifex-live-integration-static-factory.php
@@ -155,7 +155,7 @@ class Transifex_Live_Integration_Static_Factory {
 		}
 		include_once TRANSIFEX_LIVE_INTEGRATION_DIRECTORY_BASE . '/includes/transifex-live-integration-util.php';
 		$agent = Transifex_Live_Integration_Util::get_user_agent();
-		$req_escaped_fragment = (isset( $_GET['_escaped_fragment_'] )) ? $_GET['_escaped_fragment_'] : false;
+		$req_escaped_fragment = (isset( $_GET['_escaped_fragment_'] )) ? true : false;
 
 		include_once TRANSIFEX_LIVE_INTEGRATION_DIRECTORY_BASE . '/includes/lib/transifex-live-integration-prerender.php';
 		$check = Transifex_Live_Integration_Util::prerender_check( $agent, $req_escaped_fragment, $settings['generic_bot_types'], $settings['whitelist_crawlers'] );
