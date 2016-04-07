2bc675b7d23b6b48c8f077c9b6e12a34647cd514
Matthew Jackowski <mattjjacko@gmail.com>
2016-04-07 10:48:06 -0700

    fixed test

--- tests/acceptance/4-PrerenderCept.php
+++ tests/acceptance/4-PrerenderCept.php
@@ -15,6 +15,9 @@ $I->wait(5);
 $I->see('Success! Advanced SEO settings enabled.');
 $I->executeJS('jQuery("#transifex_live_settings_enable_prerender").click()');
 $I->executeJS('jQuery("#transifex_live_settings_prerender_url").val("http://192.168.99.100:32769/");');
+$I->executeJS('jQuery("#transifex_live_settings_prerender_url").trigger("input");');
+$I->dontSeeElement('#transifex_live_submit', ['disabled' => 'true']);
+
 $I->executeJS('jQuery("input#transifex_live_submit").click();');
 $I->waitForText('Your changes to the settings have been saved!', 7);
 $I->runShellCommand("curl -A 'slackbot' http://192.168.99.100:32777/blog/hello-world/");
