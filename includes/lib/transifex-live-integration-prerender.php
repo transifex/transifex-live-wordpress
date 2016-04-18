a379f10e2327f3ef0e6f4609208222870ba71cb7
Matthew Jackowski <mattjjacko@gmail.com>
2016-04-18 10:46:27 -0700

    fixes

--- includes/lib/transifex-live-integration-prerender.php
+++ includes/lib/transifex-live-integration-prerender.php
@@ -29,6 +29,8 @@ class Transifex_Live_Integration_Prerender {
 	private $prerender_cookie;
 	private $prerender_enable_response_header;
 	private $prerender_response_headers;
+	private $generic_bot_types;
+	private $whitelist_crawlers;
 
 	/*
 	 * Constructor
@@ -45,6 +47,9 @@ class Transifex_Live_Integration_Prerender {
 		$this->prerender_header_check_key = $settings['prerender_header_check_key'];
 		$this->prerender_header_check_value = $settings['prerender_header_check_value'];
 		$this->prerender_enable_response_header = (isset( $settings['prerender_enable_response_header'] )) ? true : false;
+		$this->generic_bot_types = $settings['generic_bot_types'];
+		$this->whitelist_crawlers = $settings['whitelist_crawlers'];
+		
 		$this->prerender_cookie = [ ];
 		if ( isset( $settings['prerender_cookie'] ) ) {
 			$this->prerender_cookie = json_decode( stripslashes($settings['prerender_cookie']), true );
@@ -114,7 +119,7 @@ STATUS;
 		$agent = Transifex_Live_Integration_Util::get_user_agent();
 		$req_escaped_fragment = (isset( $_GET['_escaped_fragment_'] )) ? true : false;
 		include_once TRANSIFEX_LIVE_INTEGRATION_DIRECTORY_BASE . '/includes/lib/transifex-live-integration-prerender.php';
-		$check = Transifex_Live_Integration_Util::prerender_check( $agent, $req_escaped_fragment, $this->settings['generic_bot_types'], $settings['whitelist_crawlers'] );
+		$check = Transifex_Live_Integration_Util::prerender_check( $agent, $req_escaped_fragment, $this->generic_bot_types, $this->whitelist_crawlers );
 		return $check;
 	}
 
