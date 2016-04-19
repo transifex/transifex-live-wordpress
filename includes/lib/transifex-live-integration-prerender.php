68d8dede37b6df43e7e6e689ae53428c623587ab
Matthew Jackowski <mattjjacko@gmail.com>
2016-04-19 13:41:42 -0700

    default tweaks

--- includes/lib/transifex-live-integration-prerender.php
+++ includes/lib/transifex-live-integration-prerender.php
@@ -166,7 +166,7 @@ STATUS;
 			$header = substr( $curl_response['response'], 0, $curl_response['header_size'] );
 			$body = substr( $curl_response['response'], $curl_response['header_size'] );
 			$header_lowercase = strtolower( $header );
-			$header_prerender_check = (strpos( $header_lowercase, 'x-prerender-req' )) ? true : false;
+			$header_prerender_check = (strpos( $header_lowercase, $this->prerender_header_check_key )) ? true : false;
 			$debug_html .= 'X-Prerender-Req Header check:' . $header_prerender_check . "\n";
 			$debug_html .= 'Check enabled:' . $this->enable_prerender_check . "\n";
 			if ( $header_prerender_check && $this->enable_prerender_check ) {
--- includes/transifex-live-integration-defaults.php
+++ includes/transifex-live-integration-defaults.php
@@ -111,8 +111,8 @@ class Transifex_Live_Integration_Defaults
 		'prerender_header_check_value' => 'TRUE',
 		'prerender_enable_response_header' => 0,
 		'prerender_response_headers' =>	'[ {"Expires" : "Tue, 03 Jul 2001 06:00:00 GMT"}, {"Last-Modified" : "{now} GMT"},{"Cache-Control":"max-age=0, no-cache, must-revalidate, proxy-revalidate"}]',
-		'prerender_enable_vary_header' => 0,
-		'prerender_vary_header_value' => 'User-Agent',
+		'prerender_enable_vary_header' => 1,
+		'prerender_vary_header_value' => 'X-Prerender-Req,User-Agent',
 		'prerender_enable_cookie' => 0,
 		'prerender_cookie' => '{"wordpress_test_cookie" : "WP+Cookie+check"}'
         );
