7cc6e6f70f8dd0eab66310d820897525b34ad018
Matthew Jackowski <mattjjacko@gmail.com>
2016-07-06 16:41:23 -0700

    additional fixes

--- tests/acceptance/7-StagingCept.php
+++ tests/acceptance/7-StagingCept.php
@@ -15,6 +15,11 @@ $I->amOnPage('/wp-admin/options-general.php?page=transifex-live-integration');
 $I->see('Transifex Live Translation Plugin Settings','h2');
 $I->assertTrue($I->executeJS('return (jQuery("#transifex_live_settings_api_key").val()=="2699bc66df6546008d0a14acf26732a1")?true:false;'));
 
+$I->checkOption('#transifex_live_settings_url_options_subdirectory');
+$I->executeJS('jQuery("#transifex_live_settings_url_options_subdirectory").trigger("click");');
+
+
+$I->uncheckOption('#transifex_live_settings_enable_staging');
 $I->executeJS('jQuery("#transifex_live_settings_enable_staging").click();');
 $I->executeJS('jQuery("#transifex_live_sync").click();');
 $I->wait(3);
