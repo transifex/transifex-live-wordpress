5c412562b504dfdddeec191685e7f2cbf23dc55a
Alex Psi <alexpsi256@gmail.com>
2017-01-27 16:29:19 +0200

    Testing enviroment

---
+++ docker/run.sh
@@ -0,0 +1,318 @@
+#!/bin/bash
+
+# Runtime
+# --------
+export TERM=${TERM:-xterm}
+VERBOSE=${VERBOSE:-false}
+
+# Environment
+# ------------
+DB_HOST=${DB_HOST:-'db'}
+DB_NAME=${DB_NAME:-'wordpress'}
+DB_PASS=${DB_PASS:-'root'}
+DB_PREFIX=${DB_PREFIX:-'wp_'}
+ADMIN_EMAIL=${ADMIN_EMAIL:-"admin@${DB_NAME}.com"}
+PERMALINKS=${PERMALINKS:-'/%year%/%monthnum%/%postname%/'}
+WP_DEBUG_DISPLAY=${WP_DEBUG_DISPLAY:-'true'}
+WP_DEBUG_LOG=${WB_DEBUG_LOG:-'false'}
+WP_DEBUG=${WP_DEBUG:-'false'}
+WP_VERSION=${WP_VERSION:-'latest'}
+[ "$SEARCH_REPLACE" ] && \
+  BEFORE_URL=$(echo "$SEARCH_REPLACE" | cut -d ',' -f 1) && \
+  AFTER_URL=$(echo "$SEARCH_REPLACE" | cut -d ',' -f 2) || \
+  SEARCH_REPLACE=false
+
+# WP-CLI configuration
+# ---------------------
+cat > /app/wp-cli.yml <<EOF
+apache_modules:
+  - mod_rewrite
+
+core config:
+  dbuser: root
+  dbpass: $DB_PASS
+  dbname: $DB_NAME
+  dbprefix: $DB_PREFIX
+  dbhost: $DB_HOST:3306
+  extra-php: |
+    define('WP_DEBUG', ${WP_DEBUG,,});
+    define('WP_DEBUG_LOG', ${WP_DEBUG_LOG,,});
+    define('WP_DEBUG_DISPLAY', ${WP_DEBUG_DISPLAY,,});
+
+core install:
+  url: $([ "$AFTER_URL" ] && echo "$AFTER_URL" || echo localhost:8080)
+  title: $DB_NAME
+  admin_user: root
+  admin_password: $DB_PASS
+  admin_email: $ADMIN_EMAIL
+  skip-email: true
+EOF
+
+main() {
+  h1 "Begin WordPress Installation"
+
+  # Download WordPress
+  # ------------------
+  if [ ! -f /app/wp-settings.php ]; then
+    h2 "Installing WordPress"
+    h3 "Downloading..."
+    chown -R www-data:www-data /app /var/www/html
+    WP core download --version="$WP_VERSION" |& loglevel
+    STATUS "${PIPESTATUS[0]}"
+  fi
+
+  # Wait for MySQL
+  # --------------
+  h2 "Waiting for MySQL to initialize..."
+  printf "%b " "${CYAN}${BOLD}  ->${NC} "
+  while ! mysqladmin ping --host="$DB_HOST" --password="$DB_PASS" --silent; do
+    sleep 1
+  done
+
+  h2 "Configuring WordPress"
+  h3 "Generating wp-config.php file..."
+  rm -f /app/wp-config.php
+  WP core config |& loglevel
+  STATUS "${PIPESTATUS[0]}"
+
+  h2 "Checking database"
+  check_database
+
+  # Make multisite
+  # NOTE: This will likely cause issues down the road.
+  #       Multisite should ideally be a completely separate build.
+  # ---------------
+  h2 "Checking for multisite"
+  if [ "$MULTISITE" == "true" ]; then
+    h3 "Multisite found. Enabling..."
+    WP core multisite-convert |& loglevel
+    STATUS "${PIPESTATUS[0]}"
+  else
+    h3 "Multisite not found. SKIPPING..."
+    STATUS SKIP
+  fi
+
+  h2 "Checking plugins"
+  check_plugins
+
+  h2 "Finalizing"
+  if [ ! -f /app/.htaccess ]; then
+    h3 "Generating .htaccess file"
+    if [[ "$MULTISITE" == 'true' ]]; then
+      STATUS 1
+      h3warn "Cannot generate .htaccess for multisite!"
+    else
+      WP rewrite structure "$PERMALINKS" |& loglevel
+      WP rewrite flush --hard |& loglevel
+      STATUS "${PIPESTATUS[0]}"
+    fi
+  else
+    h3 ".htaccess exists. SKIPPING..."
+    STATUS SKIP
+  fi
+
+  h3 "Adjusting file permissions"
+  groupadd -f docker && usermod -aG docker www-data
+  find /app -type d -exec chmod 755 {} \;
+  find /app -type f -exec chmod 644 {} \;
+  mkdir -p /app/wp-content/uploads
+  chmod -R 775 /app/wp-content/uploads && \
+    chown -R :docker /app/wp-content/uploads
+  STATUS $?
+
+  h1 "WordPress Configuration Complete!"
+
+  rm -f /var/run/apache2/apache2.pid
+  source /etc/apache2/envvars
+  exec apache2 -D FOREGROUND
+}
+
+
+check_database() {
+  WP core is-installed |& loglevel
+  if [ "${PIPESTATUS[0]}" == '1' ]; then
+    h3 "Creating database $DB_NAME"
+    WP db create |& loglevel
+    STATUS "${PIPESTATUS[0]}"
+
+    # If an SQL file exists in /data => load it
+    if [[ "$(find /data -name '*.sql' 2>/dev/null | wc -l)" != "0" ]]; then
+      DATA_PATH=$(find /data/*.sql | head -n 1)
+      h3 "Loading data backup from $DATA_PATH"
+
+      WP db import "$DATA_PATH" |& loglevel
+      STATUS "${PIPESTATUS[0]}"
+
+      # If SEARCH_REPLACE is set => Replace URLs
+      if [ "$SEARCH_REPLACE" != false ]; then
+        h3 "Replacing URLs"
+        REPLACEMENTS=$(WP search-replace "$BEFORE_URL" "$AFTER_URL" \
+          --skip-columns=guid | grep replacement) || \
+          ERROR $((LINENO-2)) "Could not execute SEARCH_REPLACE on database"
+        echo -ne "$REPLACEMENTS\n"
+      fi
+    else
+      h3 "No database backup found. Initializing new database"
+      WP core install |& loglevel
+      STATUS "${PIPESTATUS[0]}"
+    fi
+  else
+    h3 "Database exists. SKIPPING..."
+    STATUS SKIP
+  fi
+}
+
+check_plugins() {
+  declare -A plugins
+  local -i plugin_count=0
+  local -i i=1
+  local plugin_name
+  local plugin_url
+
+  # If $PLUGINS is not set => prune all existing plugins
+  if [[ ! "${PLUGINS-}" ]]; then
+    h3 "No plugin dependencies listed"
+    STATUS SKIP
+    h2 "Checking for orphaned plugins"
+    while read -r plugin_name; do
+      h3 "'$plugin_name' no longer needed. Pruning..."
+      WP plugin uninstall --deactivate --quiet "$plugin_name"
+      STATUS $?
+    done <<< "$(WP plugin list --field=name)"
+    return
+  fi
+
+  # Correct for cases where user forgets to add trailing comma
+  [[ "${PLUGINS:(-1)}" != ',' ]] && PLUGINS+=','
+
+  # Set $plugin_count to the total number of plugins set in $PLUGINS
+  while read -r -d,; do ((plugin_count++)); done <<< "$PLUGINS"
+
+  # Iterate over each plugin set in $PLUGINS
+  while read -r -d, plugin_name; do
+    plugin_url=  # reset to null
+
+    # If $plugin_name matches a URL using the old format => attempt to install it and continue
+    if [[ $plugin_name =~ ^https?://[www]?.+ ]]; then
+      h3warn "$plugin_name"
+      h3warn "Can't check if plugin is already installed using above format!"
+      h3warn "Switch your compose file to '[plugin-slug]http://pluginurl.com/pluginfile.zip' for better checks"
+      h3 "($i/$plugin_count) '$plugin_name' not found. Installing..."
+      WP plugin install --activate --quiet "$plugin_name"
+      STATUS $?
+      ((i++))
+      continue
+    fi
+
+    # Locally volumed plugins
+    if [[ $plugin_name =~ ^\[local\] ]]; then
+      plugins["${plugin_name##*]}"]="${plugin_name##*]}"
+      h3 "($i/$plugin_count) '${plugin_name##*]}' listed as a local volume. Activating..."
+      WP plugin activate --quiet "${plugin_name##*]}"
+      STATUS SKIP
+      ((i++))
+      continue
+    fi
+
+    # If $plugin_name matches a URL using the new format => set $plugin_name & $plugin_url
+    if [[ $plugin_name =~ ^\[.+\]https?://[www]?.+ ]]; then
+      plugin_url=${plugin_name##\[*\]}
+      plugin_name="$(echo "$plugin_name" | grep -oP '\[\K(.+)(?=\])')"
+    fi
+
+    plugin_url=${plugin_url:-$plugin_name}
+
+    if WP plugin is-installed "$plugin_name"; then
+      h3 "($i/$plugin_count) '$plugin_name' found. SKIPPING..."
+      STATUS SKIP
+    else
+      h3 "($i/$plugin_count) '$plugin_name' not found. Installing..."
+      WP plugin install --activate --quiet "$plugin_url"
+      STATUS $?
+      # Pretty much guarenteed to need/want 'restful' if you are using 'rest-api'
+      if [ "$plugin_name" == 'rest-api' ]; then
+        h3 "($i.5/$plugin_count) Installing 'restful' WP-CLI package..."
+        wp package install wp-cli/restful --quiet --allow-root
+        STATUS $?
+      fi
+    fi
+
+    plugins[$plugin_name]=$plugin_url
+    ((i++))
+  done <<< "$PLUGINS"
+
+  h2 "Checking for orphaned plugins"
+  while read -r plugin_name; do
+    if [[ ! ${plugins[$plugin_name]} ]]; then
+      h3 "'$plugin_name' no longer needed. Pruning..."
+      WP plugin uninstall --deactivate --quiet "$plugin_name"
+      STATUS $?
+    fi
+  done <<< "$(WP plugin list --field=name)"
+}
+
+
+# Helpers
+# --------------
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+PURPLE='\033[0;34m'
+CYAN='\033[0;36m'
+BOLD='\E[1m'
+NC='\033[0m'
+
+h1() {
+  local len=$(($(tput cols)-1))
+  local input=$*
+  local size=$(((len - ${#input})/2))
+
+  for ((i = 0; i < len; i++)); do echo -ne "${PURPLE}${BOLD}="; done; echo ""
+  for ((i = 0; i < size; i++)); do echo -n " "; done; echo -e "${NC}${BOLD}$input"
+  for ((i = 0; i < len; i++)); do echo -ne "${PURPLE}${BOLD}="; done; echo -e "${NC}"
+}
+
+h2() {
+  echo -e "${ORANGE}${BOLD}==>${NC}${BOLD} $*${NC}"
+}
+
+h3() {
+  printf "%b " "${CYAN}${BOLD}  ->${NC} $*"
+}
+
+h3warn() {
+  printf "%b " "${RED}${BOLD}  [!]|${NC} $*" && echo ""
+}
+
+STATUS() {
+  local status=$1
+  if [[ $1 == 'SKIP' ]]; then
+    echo ""
+    return
+  fi
+  if [[ $status != 0 ]]; then
+    echo -e "${RED}✘${NC}"
+    return
+  fi
+  echo -e "${GREEN}✓${NC}"
+}
+
+ERROR() {
+  echo -e "${RED}=> ERROR (Line $1): $2.${NC}";
+  exit 1;
+}
+
+WP() {
+  sudo -u www-data wp "$@"
+}
+
+loglevel() {
+  [[ "$VERBOSE" == "false" ]] && return
+  local IN
+  while read -r IN; do
+    echo "$IN"
+  done
+}
+
+main
--- readme.md
+++ readme.md
@@ -1,114 +1,15 @@
 [![Build Status](https://travis-ci.org/transifex/transifex-live-wordpress.svg?branch=devel)](https://travis-ci.org/transifex/transifex-live-wordpress)
 
-#International SEO by Transifex
+# International SEO by Transifex
 
+This branch creates a dockerized development environment in order to test the transifex-live-wordpress plugin
 
-[What is Transifex?](https://www.transifex.com/product/?utm_source=github&utm_medium=web&utm_campaign=tx-live-wp-plugin)
+## Instructions
 
+* ```git checkout docker```
+* ```mv docker ../transifex-wordpress-docker```
+* ```git checkout devel```
+* ```cd ../transifex-wordpress-docker```
+* ```docker-compose up```
 
-
-== Description ==
-
-The Transifex International SEO is a WordPress plugin designed to be used with Transifex.  In order to use Transifex, you will need to [sign up here for an account](https://www.transifex.com/signup/?utm_source=github&utm_medium=web&utm_campaign=tx-live-wp-plugin). Please note that a Transifex Live API key is required in order to use the plugin.  More information about how to obtain a key can be found in the [plugin documentation here](http://docs.transifex.com/integrations/wordpress/#getting-your-transifex-live-api-key/?utm_source=github&utm_medium=web&utm_campaign=tx-live-wp-plugin) if you don't have a key yet.  
-
-== Features ==
-
-* Integrates Transifex into WordPress
-* Adds support for localized language URLs either by subdomain or subdirectory.
-* Adds support rewriting all URLs on the page
-* Automatically adds hreflang tags to your pages.
-* Adds supports for using an external prerendered server for SEO purposes
-* Works with WordPress multisite
-
-== Get Involved ==
-
-Developers can contribute via this repository. Please send a Pull Request.
-
-Translators can contribute new languages to this plugin or our other WordPress plugins through [Transifex](https://www.transifex.com/projects/p/transifex-live/).
-
-== Minimum Requirements ==
-
-* WordPress 3.5.2 or greater
-* PHP version 5.4.0 or greater
-* MySQL version 5.0 or greater
-
-=== Installation ===
-
-== Automatic Installation ==
-
-To do an automatic install of Transifex Live, log in to your WordPress admin panel, navigate to the Plugins menu and click Add New.
-
-In the search field type "Transifex Live" and click Search Plugins. Once you’ve found the plugin you can view details about it such as the point release, rating, and description. You can install it by simply clicking Install Now. After clicking that link you will be asked if you’re sure you want to install the plugin. Click "yes" and WordPress will automatically complete the installation.
-After installation a new menu setting option will appear called 'Transifex Live'.  You will need to complete the admin form before the plugin will become active.
-
-== Manual Installation ==
-
-Manual installation involves downloading the plugin and uploading it to your webserver via your favorite FTP application.
-
-1. Download the plugin file to your computer and unzip it
-2. Using an FTP program, or your hosting control panel, upload the unzipped plugin folder to your WordPress installation’s wp-content/plugins/ directory.
-3. Activate the plugin from the Plugins menu within the WordPress admin.
-After installation a new menu setting option will appear called 'Transifex Live'.  You will need to complete the admin form before the plugin will become active.
-
-== Upgrading ==
-
-Automatic updates should work like a charm; as always though, ensure you backup your site just in case.
-
-== Plugin Translations ==
-
-The WP-Translations project for plugin translations can be found [here](https://www.transifex.com/wp-translations/transifex-live/transifex-live/).
-
-== Changelog ==
-
-= 1.3.9 =
-Cosmetic changes to plugin copy and WordPress.org assets
-
-= 1.3.8 =
-Added staging checkbox to admin page
-
-= 1.3.7 =
-Fix to allow custom hreflang code and enhanced subdomain language detection
-
-= 1.3.6 =
-Patch release for rewrite fix when locale is in url
-
-= 1.3.5 =
-Patch release for improved static front page support
-
-= 1.3.4 =
-Minor patch release, cleared up some minor warning issues
-
-= 1.3.3 =
-A few minor fixes.  Revised admin UI
-
-= 1.3.2 =
-Added additional Prerender options for caching
-
-= 1.3.1 =
-Patch for Prerender logic
-
-= 1.3.0 =
-Fixed support for PHP 5.4
-Fixed hreflang tag output for subdirectories
-Improved admin UI
-Added picker support that respects locale
-Additional admin API key validation
-Added Prerendering capability
-Fixed some timing issues with the WP loop
-
-= 1.2.0 =
-Added support for subdomains
-Added reverse lookups for many link types
-
-= 1.1.0 =
-Cleaned up readme and notes
-Fixed brittle js ordering and namespace
-Removed settings that can now be controlled in Transifex Live dashboard
-Initial implementation of SEO and lang urls
-SEO and lang urls feature switch set to off
-Removing staging option (use Transifex dashboard to control it)
-SEO and lang urls and HREFLANG enabled
-Custom language picker color options removed
-
-= 1.0.0 =
-Full release.  Restructured plugin to follow boilerplate.  Added unit tests.
+If everything goes well, you will have a wordpress instance listening to port localhost:8080 with the transifex plugin activated from the code at ../transifex-live-wordpress (username: root/ password: root)
