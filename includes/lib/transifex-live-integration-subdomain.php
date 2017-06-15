9cbc846a1fbc2e27b983be08abd1c1800bbe6e0d
Sofia Margariti <sofia@transifex.com>
2017-06-15 16:33:04 +0300

    Support localized static front page for subdomains

--- includes/lib/transifex-live-integration-subdomain.php
+++ includes/lib/transifex-live-integration-subdomain.php
@@ -55,13 +55,45 @@ class Transifex_Live_Integration_Subdomain {
 	function parse_query_hook( $query ) {
 		$m = array();
 		Plugin_Debug::logTrace('checking subdomain:'.$this->subdomain_pattern);
+        Plugin_Debug::logTrace(site_url());
 		if ( preg_match( $this->subdomain_pattern, site_url(), $m ) ) {
 			Plugin_Debug::logTrace($m);
 			$query->query_vars['lang'] = $m[1];
 		} else {
-			$query->query_vars['lang'] = $this->source_language;
+            if ( isset($query->query['lang']) ) {
+                $query->query_vars['lang'] = $query->query['lang'];
+            } else {
+                $query->query_vars['lang'] = $this->source_language;
+            }
 		}
 		return $query;
+	}
+
+	/*
+	 * WP parse_query filter,additional logic to support localized static frontpages
+	 * @param array $query WP query object.
+	 * @return array Returns the filtered query object
+	 */
+	function parse_query_root_hook( $query ) {
+		global $wp_query;
+		$check_for_lang = ($query->get( 'lang' ) !== $this->source_language) ? true : false;
+		$check_page = (null !== $query->get( 'page' ) ) ? true : false;
+		$check_pagename = ($query->get( 'pagename' )) ? true : false;
+		$check_page_on_front = (get_option( 'page_on_front' )) ? true : false;
+		if ( $check_for_lang && $check_page_on_front && $wp_query->is_home ) {
+			if ( $check_page && $check_pagename ) {
+                Plugin_Debug::logTrace('check page and check pagename');
+				$wp_query->is_page = false;
+				$wp_query->is_home = true;
+				$wp_query->is_posts_page = true;
+			} else {
+                Plugin_Debug::logTrace('else check page and check pagename');
+				$wp_query->is_page = true;
+				$wp_query->is_home = false;
+				$wp_query->is_singular = true;
+				$query->set( 'page_id', get_option( 'page_on_front' ) );
+			}
+		}
 	}
 
 }
