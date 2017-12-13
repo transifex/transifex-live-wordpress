ee23df5846b5af0198078351a80ae65d010389ee
Sofia Margariti <sofia@transifex.com>
2017-12-13 13:56:05 +0200

    add X-Transifex-Lang value in the debug html comment

--- includes/lib/transifex-live-integration-prerender.php
+++ includes/lib/transifex-live-integration-prerender.php
@@ -166,6 +166,7 @@ STATUS;
 		$lang = urlencode($_SERVER['HTTP_X_TRANSIFEX_LANG']);
 
 		$debug_html = '<!--' . "\n";
+		$debug_html .= 'HTTP_X_TRANSIFEX_LANG: ' . $lang . "\n";
 		$page_url = home_url( $wp->request );
 		if ( !empty($lang) ) {
 			$page_url = Transifex_Live_Integration_Util::replace_lang_subdomain($page_url, $lang);
