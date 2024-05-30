30fa900ea170d9358816157199d7f36f04c63bf0
Jack Dimas <giannosdim@transifex.com>
2024-05-30 22:07:55 +0300

    Custom post type, remove slug from URL
    Example of removing a custom post type's slug from the generated URL.

--- transifex-live-integration-main.php
+++ transifex-live-integration-main.php
@@ -162,6 +162,29 @@ class Transifex_Live_Integration {
 		$subdirectory = Transifex_Live_Integration_Static_Factory::create_subdirectory( $settings, $rewrite_options );
 		($subdirectory) ? Plugin_Debug::logTrace( 'subdirectory created' ) : Plugin_Debug::logTrace( 'subdirectory skipped' );
 		if ( $subdirectory ) {
+			function na_remove_slug( $post_link, $post, $leavename ) {
+
+				if ( 'movies' != $post->post_type || 'publish' != $post->post_status ) {
+					return $post_link;
+				}
+
+				$post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+
+				return $post_link;
+			}
+			add_filter( 'post_type_link', 'na_remove_slug', 10, 3 );
+
+			function na_parse_request( $query ) {
+
+				if ( ! $query->is_main_query() || 2 != count( $query->query ) || ! isset( $query->query['page'] ) ) {
+					return;
+				}
+
+				if ( ! empty( $query->query['name'] ) ) {
+					$query->set( 'post_type', array( 'post', 'movies', 'page' ) );
+				}
+			}
+			add_action( 'pre_get_posts', 'na_parse_request' );
 
 			// Adds 'lang' to query_vars for use in the template.
 			add_action( 'parse_query', [ $subdirectory, 'parse_query_hook' ] );
